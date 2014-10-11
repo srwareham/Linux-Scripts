f68d80d010c012df021b9c0aeb03a44bf3505745
Sean Wareham <srwareham@hotmail.com>
2014-10-11 17:13:07 -0400

    Added invalid directory error handling

--- ffmpeg-scripts/utils.bash
+++ ffmpeg-scripts/utils.bash
@@ -1,7 +1,17 @@
 #!/bin/bash
+
+#------------File System Utils---------------
 # Create a directory if needed
 createDirIfNeeded() {
     if [ ! -d "$1" ]; then
         mkdir "$1"
+    fi
+}
+
+#-----------Input Verification Utils---------
+validateDirectory() {
+    if [ ! -d "$1" ]; then
+        echo \""$1"\" is not a valid directory. Terminating program.
+        exit -1;
     fi
 }
