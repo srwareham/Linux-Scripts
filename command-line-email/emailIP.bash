160b57d2dd116833a3680033546e7257b7223f6e
srwareham <srwareham@hotmail.com>
2015-01-16 21:06:51 -0500

    Added warning to emailIP.bash

--- command-line-email/emailIP.bash
+++ command-line-email/emailIP.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
 toEmail="address@server.com"
 IP=$(curl -s checkip.dyndns.org|sed -e 's/.*Current IP Address: //' -e 's/<.*$//')
+# NOTE: most modern email services will prohibit the mail implementation used, special settings will likely need to be made to recieve them
 echo "$IP" | mail -s "IP Address Update" "$toEmail"
